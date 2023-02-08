a79f41bb71f351b487c0ce3a7a3affeae73f3945
Jamie Perkins <jamie.perkins@gmail.com>
2023-02-08 12:12:52 -0700

    fix base href, new favicon

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,7 +20,7 @@ import { MapoverModule } from './mapover/mapover.module';
     SharedModule
   ],
   providers: [
-    { provide: APP_BASE_HREF, useValue: '/mapover'}
+    { provide: APP_BASE_HREF, useValue: '/'}
   ],
   bootstrap: [AppComponent]
 })
@@ -30,7 +30,7 @@ export class AppModule { }
   for local dev:
   { provide: APP_BASE_HREF, useValue: '/'}
 
-  for prod:
+  for prod (if deployed to /mapover):
   { provide: APP_BASE_HREF, useValue: '/mapover'}
   prod build:
   ng build --prod --deploy-url=mapover
